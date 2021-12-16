1b01e37ab165280b134b8f2af19a4b1d30cbb04e
alokkumar-dev <alokshakya2601@gmail.com>
2021-12-16 11:43:23 +0530

    add a js file and modify html and css file

---
+++ addToCart.js
@@ -0,0 +1,134 @@
+
+var cartData = [
+    {
+        prodImage : "https://cdn.shopify.com/s/files/1/1743/7443/products/1518253-Revolution-Relove--Baby-Tint-Coral--Lip---Cheek-Tint_2-1080x1080-5b3e89b6-be83-423b-9264-18c5a2fe396a_1024x1024.jpg?v=1637413249",
+        name : "Revolution Relove",
+        desc : "relution Revolution Relution Relove Bavy Tint Coral Lip & Cheek Tint",
+        deletPrice : "₹ 499",
+        price : "₹ 324"
+    },
+    {
+        prodImage : "https://cdn.shopify.com/s/files/1/1743/7443/products/1518246--Revolution-Relove--Gloss-Up-Eye-Gloss_2-1080x1080-8c994c46-0ee8-4bcf-af81-b33148e56ea3_1024x1024.jpg?v=1637412657",
+        name : "Revolution Relove",
+        desc : "Revolution Relution Relove Bavy Tint Coral Lip & Cheek Tint",
+        deletPrice : "₹ 499",
+        price : "₹ 324"
+    },
+    {
+        prodImage : "https://cdn.shopify.com/s/files/1/1743/7443/products/1518239--Revolution-Relove--Prime-Up--Perfecting-Eye-Prime_2-1080x1080-946eb39a-a31d-4c06-9149-4a28765f74cb_1024x1024.jpg?v=1637411865",
+        name : "Revolution Relove",
+        desc : "Revolution Relution Relove Bavy Tint Coral Lip & Cheek Tint",
+        deletPrice : "₹ 499",
+        price : "₹ 324"
+    },
+    {
+        prodImage : "https://cdn.shopify.com/s/files/1/1743/7443/products/1518185-Relove-EyeLight-Shine-MetallicEyeshadows-1-1080x1080-6cab39b6-5ce2-4161-a583-517d23a5195c_1024x1024.jpg?v=1637409282",
+        name : "Revolution Relove",
+        desc : "Revolution Relution Relove Bavy Tint Coral Lip & Cheek Tint",
+        deletPrice : "₹ 499",
+        price : "₹ 324"
+    },
+    {
+        prodImage : "https://cdn.shopify.com/s/files/1/1743/7443/products/RelovebyRevolutionLipliner_1024x1024.jpg?v=1637405589",
+        name : "Revolution Relove",
+        desc : "Revolution Relution Relove Bavy Tint Coral Lip & Cheek Tint",
+        deletPrice : "₹ 299",
+        price : "₹ 194"
+    },
+    {
+        prodImage : "https://cdn.shopify.com/s/files/1/1743/7443/products/1518543-RELOVE-BRUSHQUEEN_DETAIL-EYE_01-1080x1080-b291aa41-af19-4ece-96a7-3896a0143f15_1024x1024.jpg?v=1637324162",
+        name : "Revolution Relove",
+        desc : "Revolution Relution Relove Bavy Tint Coral Lip & Cheek Tint",
+        deletPrice : "₹ 299",
+        price : "₹ 194"
+    },
+    {
+        prodImage : "https://cdn.shopify.com/s/files/1/1743/7443/products/1518253-Revolution-Relove--Baby-Tint-Coral--Lip---Cheek-Tint_2-1080x1080-5b3e89b6-be83-423b-9264-18c5a2fe396a_1024x1024.jpg?v=1637413249",
+        name : "Revolution Relove",
+        desc : "Revolution Relution Relove Bavy Tint Coral Lip & Cheek Tint",
+        deletPrice : "₹ 99",
+        price : "₹ 324"
+    },
+    {
+        prodImage : "https://cdn.shopify.com/s/files/1/1743/7443/products/1518253-Revolution-Relove--Baby-Tint-Coral--Lip---Cheek-Tint_2-1080x1080-5b3e89b6-be83-423b-9264-18c5a2fe396a_1024x1024.jpg?v=1637413249",
+        name : "Revolution Relove",
+        desc : "Revolution Relution Relove Bavy Tint Coral Lip & Cheek Tint",
+        deletPrice : "₹ 499",
+        price : "₹ 324"
+    },
+    {
+        prodImage : "https://cdn.shopify.com/s/files/1/1743/7443/products/1518253-Revolution-Relove--Baby-Tint-Coral--Lip---Cheek-Tint_2-1080x1080-5b3e89b6-be83-423b-9264-18c5a2fe396a_1024x1024.jpg?v=1637413249",
+        name : "Revolution Relove",
+        desc : "Revolution Relution Relove Bavy Tint Coral Lip & Cheek Tint",
+        deletPrice : "₹ 499",
+        price : "₹ 324"
+    },
+    {
+        prodImage : "https://cdn.shopify.com/s/files/1/1743/7443/products/1518253-Revolution-Relove--Baby-Tint-Coral--Lip---Cheek-Tint_2-1080x1080-5b3e89b6-be83-423b-9264-18c5a2fe396a_1024x1024.jpg?v=1637413249",
+        name : "Revolution Relove",
+        desc : "Revolution Relution Relove Bavy Tint Coral Lip & Cheek Tint",
+        deletPrice : "₹ 499",
+        price : "₹ 324"
+    },
+    {
+        prodImage : "https://cdn.shopify.com/s/files/1/1743/7443/products/1518253-Revolution-Relove--Baby-Tint-Coral--Lip---Cheek-Tint_2-1080x1080-5b3e89b6-be83-423b-9264-18c5a2fe396a_1024x1024.jpg?v=1637413249",
+        name : "Revolution Relove",
+        desc : "Revolution Relution Relove Bavy Tint Coral Lip & Cheek Tint",
+        deletPrice : "₹ 499",
+        price : "₹ 324"
+    },
+    {
+        prodImage : "https://cdn.shopify.com/s/files/1/1743/7443/products/1518253-Revolution-Relove--Baby-Tint-Coral--Lip---Cheek-Tint_2-1080x1080-5b3e89b6-be83-423b-9264-18c5a2fe396a_1024x1024.jpg?v=1637413249",
+        name : "Revolution Relove",
+        desc : "Revolution Relution Relove Bavy Tint Coral Lip & Cheek Tint",
+        deletPrice : "₹ 499",
+        price : "₹ 324"
+    },
+];
+
+cartData.map(function (event){
+    var imgDiv = document.createElement("div")
+    imgDiv.setAttribute("class", "prodImagediv")
+
+    var image = document.createElement("img");
+    imgDiv.append(image);
+
+    var prodDesDiv = document.createElement("div");
+    prodDesDiv.setAttribute("class", "prodDesc");
+
+    var h3 = document.createElement("h3");
+    h3.setAttribute("class", "Revolution Relove")
+
+    var h4 =document.createElement("h4");
+    h4.setAttribute("class", "relution")
+    
+    var priceDiv = document.createElement("div");
+    priceDiv.setAttribute("class", "price");
+
+    var delPrice = document.createElement("h1");
+    delPrice.setAttribute("class", "deletPrice")
+
+    var mainPrice = document.createElement("h1");
+    mainPrice.setAttribute("class", "mainPrice")
+
+    priceDiv.append(delPrice, mainPrice);
+    prodDesDiv.append(priceDiv,h3,h4);
+
+    var button = document.createElement("button");
+    button.setAttribute("class", "addToCartBtn");
+    button.textContent = "ADD TO CART";
+
+    image.setAttribute("src" , event.prodImage);
+    h3.textContent = event.name;
+    h4.textContent = event.desc;
+    delPrice.textContent = event.deletPrice;
+    mainPrice.textContent = event.price;
+
+    var itemCart = document.createElement("div");
+    itemCart.setAttribute("class", "productDiv")
+
+    itemCart.append(imgDiv,prodDesDiv,button);
+
+    document.querySelector(".allProducts").append(itemCart);
+
+});
